b1f5cf18679703265f72e24eb3b4533ac0374c07
RJaime-jr <Ricoa.jaime@outlook.com>
2022-08-29 22:03:51 -0700

    Fixed some page styling

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ const grid = document.createElement('div');
 grid.classList.add('game-grid');
 
 
+const sayHello = (e) => {
+    console.log("hello!");
+}
+
 function createSquare() {
     const square = document.createElement('div');
+    const text = document.createElement('div');
     square.classList.add('squares');
-
-
+    text.classList.add('square-text');
+    square.onclick = sayHello;
     grid.appendChild(square);
+    square.appendChild(text);
+    text.innerText = "X";
+
 }
 
 gameBoard.appendChild(grid);
@@ -19,3 +27,4 @@ for (let i = 0; i < 9; i++) {
     createSquare();
 
 }
+
